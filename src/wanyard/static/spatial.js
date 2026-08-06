4750c1c133dcae74b843f859238ce192e36766ac
blowhacker <n@vain.in>
2026-08-06 19:50:18 +0100

    Correct spatial scene handedness

--- src/wanyard/static/spatial.js
+++ src/wanyard/static/spatial.js
@@ -170,7 +170,7 @@
     const center = low.map((value, axis) => (value + high[axis]) / 2);
     const scale = Math.max(...low.map((value, axis) => high[axis] - value)) || 1;
     for (let index = 0; index < positions.length; index += 3) {
-      positions[index] = (positions[index] - center[0]) / scale * 2;
+      positions[index] = -(positions[index] - center[0]) / scale * 2;
       positions[index + 1] = -(positions[index + 1] - center[1]) / scale * 2;
       positions[index + 2] = (positions[index + 2] - center[2]) / scale * 2;
     }
